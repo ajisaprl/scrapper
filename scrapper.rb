9670bae8700b1e6a244f2c42518bfc2c3f293544
Aziz Apriyanto <aziz.apriyanto@bukalapak.com>
2020-05-07 23:48:21 +0700

    user choose marketplace

--- scrapper.rb
+++ scrapper.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/ruby
+
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
@@ -6,22 +8,35 @@ require 'csv'
 
 def scrapper
 	page = 1
-	last_page = 5
+	last_page = 2
 	csv = CSV.open("data.csv", "a+")
 	csv << ["Product Name", "Price", "Location", "URL"]
+
+	puts "Choose Marketplace: "
+	puts "[1] Bukalapak"
+	puts "[2] Tokopedia"
+	marketplace = gets.chomp
+	puts "Scrapping begin"
 	while page <= last_page
-		url = "https://www.bukalapak.com/products?page=#{page}&search%5Bkeywords%5D=kemeja"
-		doc = Nokogiri::HTML(open(url))
-		puts "Scrapping page #{page}..."
-		product_cards = doc.search('div.product-card')
-		product_cards.each do |product_card|
-			product = {
-				productName: product_card.css('a.product__name').text,
-				productPrice: product_card.css('div.product-price')[0].attributes["data-reduced-price"].text,
-				storeLocation: product_card.css('div.user-city').text,
-				url: "https://www.bukalapak.com" + product_card.search('a')[0].attributes["href"].value
-			}
-			csv << ["#{product[:productName]}", "#{product[:productPrice]}","#{product[:storeLocation]}","#{product[:url]}"]
+		case marketplace
+		when "1"
+			url = "https://www.bukalapak.com/products?page=#{page}&search%5Bkeywords%5D=kemeja"
+			doc = Nokogiri::HTML(open(url))
+			puts "Scrapping page #{page}..."
+			product_cards = doc.search('div.product-card')
+			product_cards.each do |product_card|
+				product = {
+					productName: product_card.css('a.product__name').text,
+					productPrice: product_card.css('div.product-price')[0].attributes["data-reduced-price"].text,
+					storeLocation: product_card.css('div.user-city').text,
+					url: "https://www.bukalapak.com" + product_card.search('a')[0].attributes["href"].value
+				}
+				csv << ["#{product[:productName]}", "#{product[:productPrice]}","#{product[:storeLocation]}","#{product[:url]}"]
+			end
+		when "2"
+			puts "Tokopedia is not available yet"
+		else
+			puts "Not available yet"
 		end
 		page += 1
 	end
