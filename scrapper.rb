a15f34a7023e596d799cc5cf738fdfad1b4fcec2
Aziz Apriyanto <aziz.apriyanto@bukalapak.com>
2020-05-07 21:29:41 +0700

    extract data to csv

--- scrapper.rb
+++ scrapper.rb
@@ -2,32 +2,30 @@ require 'nokogiri'
 require 'httparty'
 require 'byebug'
 require 'open-uri'
+require 'csv'
 
 def scrapper
 	page = 1
 	last_page = 5
-
+	csv = CSV.open("data.csv", "a+")
+	csv << ["Product Name", "Price", "Location", "URL"]
 	while page <= last_page
 		url = "https://www.bukalapak.com/products?page=#{page}&search%5Bkeywords%5D=kemeja"
 		doc = Nokogiri::HTML(open(url))
-		puts "Page: #{page}"
-		puts ''
-		product_listings = doc.search('div.product-card')
-		product_listings.each do |product_listing|
+		puts "Scrapping page #{page}..."
+		product_cards = doc.search('div.product-card')
+		product_cards.each do |product_card|
 			product = {
-				productName: product_listing.css('a.product__name').text,
-				productPrice: product_listing.css('div.product-price')[0].attributes["data-reduced-price"].text,
-				storeLocation: product_listing.css('div.user-city').text,
-				url: "https://www.bukalapak.com" + product_listing.search('a')[0].attributes["href"].value
+				productName: product_card.css('a.product__name').text,
+				productPrice: product_card.css('div.product-price')[0].attributes["data-reduced-price"].text,
+				storeLocation: product_card.css('div.user-city').text,
+				url: "https://www.bukalapak.com" + product_card.search('a')[0].attributes["href"].value
 			}
-			puts "Product Name: #{product[:productName]}"
-			puts "Product Price: #{product[:productPrice]}"
-			puts "Store Location: #{product[:storeLocation]}"
-			puts "Product URL: #{product[:url]}"
-			puts '#########################'
+			csv << ["#{product[:productName]}", "#{product[:productPrice]}","#{product[:storeLocation]}","#{product[:url]}"]
 		end
 		page += 1
 	end
+	puts "Scrapped data in data.csv"
 end
 
 scrapper
