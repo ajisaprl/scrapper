8d008f9c3d39ddcfe48897a263a38a973e7e820c
Aziz Apriyanto <azizapriyanto@tanihub.com>
2021-11-06 06:58:36 +0700

    implement bot

--- telegram_bot.rb
+++ telegram_bot.rb
@@ -2,22 +2,22 @@ require 'telegram/bot'
 require 'pry'
 require './scrapper'
 
-@token = '2007354675:AAGwCTKJjtzwpjbkz6Z8buXYjQk5_CvjHlQ'
+$token = '2007354675:AAGwCTKJjtzwpjbkz6Z8buXYjQk5_CvjHlQ'
 @keyword = ''
 @marketplace = ''
 
 def get_keyword
-	Telegram::Bot::Client.run(@token) do |bot|
+	Telegram::Bot::Client.run($token) do |bot|
 		bot.listen do |message|
-			question = 'Please input product keyword with format: "/key product_keyword"'
-			bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}. #{question}")
+			question = 'Please input product keyword with format: `/key Product Name`'
+			bot.api.send_message(chat_id: message.chat.id, text: "#{question}", parse_mode: 'markdown')
 			break
 		end
 	end
 end
 
 def select_marketplace
-	Telegram::Bot::Client.run(@token) do |bot|
+	Telegram::Bot::Client.run($token) do |bot|
 		bot.listen do |message|
 				kb = [
 					Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Bukalapak'),
@@ -31,23 +31,38 @@ def select_marketplace
 	end
 end
 
-Telegram::Bot::Client.run(@token) do |bot|
+Telegram::Bot::Client.run($token) do |bot|
 	bot.listen do |message|
-		case
-		when message.text.eql?('/start')
-			get_keyword
-		when message.text.include?('/key')
-			@keyword = message.text.gsub('/key', '').lstrip
-			select_marketplace
-		when ['Bukalapak', 'Tokopedia', 'Shopee'].any? { |text| text.include? message.text }
-			@marketplace = message.text
-			Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
-		end
-		unless @marketplace.empty? || @keyword.empty?
-			Scrapper.scrapper(@keyword, @marketplace)
-			bot.api.send_document(chat_id: message.chat.id, reply_to_message_id: message.message_id, document: Faraday::UploadIO.new('./data.csv', 'csv'))
-			@keyword = ''
-			@marketplace = ''
+		case message
+		when Telegram::Bot::Types::Message
+			case
+			when message.text.eql?('/start')
+				get_keyword
+			when message.text.include?('/key')
+				@keyword = message.text.gsub('/key', '').lstrip
+				select_marketplace
+			when ['Bukalapak', 'Tokopedia', 'Shopee'].any? { |text| text.include? message.text }
+				@marketplace = message.text
+				Telegram::Bot::Types::ReplyKeyboardRemove.new(remove_keyboard: true)
+			end
+			unless @marketplace.empty? || @keyword.empty?
+				Scrapper.scrapper(@keyword, @marketplace)
+				bot.api.send_chat_action(chat_id: message.chat.id, action: 'upload_document')
+				retries = 0
+				begin
+					bot.api.send_document(chat_id: message.chat.id, reply_to_message_id: message.message_id, document: Faraday::UploadIO.new("./data#{message.from.id}.csv", 'csv'))
+				rescue Telegram::Bot::Exceptions::ResponseError => e
+					p 'failed at empty file'
+					Scrapper.scrapper(@keyword, @marketplace)
+					retry if (retries += 1) < 5
+					raise e.message if retries == 5
+				end
+				@keyword = ''
+				@marketplace = ''
+			end
+		when Telegram::Bot::Types::ChatMemberUpdated
+			p 'KENANICH'
+			next
 		end
 	end
 end
